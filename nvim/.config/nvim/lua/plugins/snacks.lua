cd3d91621541cc082f6201c7c811f8582b16a311
Paulius Putna <paulius@putna.dev>
2025-02-22 16:09:10 +0200

    Enable hidden files for most pickers

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -1,50 +1,60 @@
 return {
-  "folke/snacks.nvim",
-  opts = {
-    picker = {
-      sources = {
-        buffers = {
-          on_show = function()
-            vim.cmd.stopinsert()
-          end,
-          finder = "buffers",
-          format = "buffer",
-          hidden = false,
-          unloaded = true,
-          current = true,
-          sort_lastused = true,
-          win = {
-            input = {
-              keys = {
-                ["d"] = "bufdelete",
-              },
+    "folke/snacks.nvim",
+    opts = {
+        picker = {
+            sources = {
+                buffers = {
+                    on_show = function()
+                        vim.cmd.stopinsert()
+                    end,
+                    finder = "buffers",
+                    format = "buffer",
+                    hidden = false,
+                    unloaded = true,
+                    current = true,
+                    sort_lastused = true,
+                    win = {
+                        input = {
+                            keys = {
+                                ["d"] = "bufdelete",
+                            },
+                        },
+                        list = { keys = { ["d"] = "bufdelete" } },
+                    },
+                },
+                explorer = {
+                    hidden = true,
+                    -- ignored = true,
+                },
+                files = {
+                    hidden = true,
+                    -- ignored = true,
+                },
+                grep = {
+                    hidden = true,
+                },
             },
-            list = { keys = { ["d"] = "bufdelete" } },
-          },
-        },
-        explorer = {},
-      },
-      matcher = {
-        frequency = true,
-      },
-      layout = {
-        preset = "ivy",
-        cycle = false,
-      },
-      win = {
-        input = {
-          keys = {
-            ["<Esc>"] = { "close", mode = { "n", "i" } },
+            matcher = {
+                frequency = true,
+            },
+            layout = {
+                preset = "ivy",
+                cycle = false,
+            },
+            win = {
+                input = {
+                    keys = {
+                        ["<Esc>"] = { "close", mode = { "n", "i" } },
 
-            ["J"] = { "preview_scroll_down", mode = { "i", "n" } },
-            ["K"] = { "preview_scroll_up", mode = { "i", "n" } },
-            ["H"] = { "preview_scroll_left", mode = { "i", "n" } },
-            ["L"] = { "preview_scroll_right", mode = { "i", "n" } },
-            ["<c-j>"] = { "", mode = { "i", "n" } },
-            ["<c-k>"] = { "", mode = { "i", "n" } },
-          },
+                        ["J"] = { "preview_scroll_down", mode = { "i", "n" } },
+                        ["K"] = { "preview_scroll_up", mode = { "i", "n" } },
+                        ["H"] = { "preview_scroll_left", mode = { "i", "n" } },
+                        ["L"] = { "preview_scroll_right", mode = { "i", "n" } },
+                        ["<c-j>"] = { "", mode = { "i", "n" } },
+                        ["<c-k>"] = { "", mode = { "i", "n" } },
+                    },
+                },
+            },
         },
-      },
     },
-  },
 }
