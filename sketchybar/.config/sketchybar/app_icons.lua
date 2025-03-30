d98a5ee3728b7fb4c057c815f5ab162cff9a2e9f
Paulius Putna <paulius@putna.dev>
2025-03-30 22:37:27 +0300

    fix(sketchybar): fix zen browser icon

--- sketchybar/.config/sketchybar/app_icons.lua
+++ sketchybar/.config/sketchybar/app_icons.lua
@@ -306,7 +306,7 @@ return {
 	["Yuque"] = ":yuque:",
 	["语雀"] = ":yuque:",
 	["Zed"] = ":zed:",
-	["Zen Browser"] = ":zen_browser:",
+	["Zen"] = ":zen_browser:",
 	["Zeplin"] = ":zeplin:",
 	["zoom.us"] = ":zoom:",
 	["Zotero"] = ":zotero:",
