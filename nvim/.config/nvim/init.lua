d2f3782e276fb94f6c6054407cd2e20a4647dc04
Paulius Putna <paulius@putna.dev>
2026-04-20 14:11:37 +0300

    feat(nvim): remove hardtime, add vitest for neotest, add md lint rule check

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,2 +1,5 @@
+-- Use system compiler to avoid macOS code signing issues with Nix-built binaries.
+vim.env.CC = "/usr/bin/cc"
+
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
